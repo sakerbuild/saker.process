103398f257747009b08b2cac0866ecf5f8333719
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-02-04 09:19:48 +0100

    Redirect legacy process stdin to NULL

--- src/main/saker/process/impl/JavaSakerProcessBuilder.java
+++ src/main/saker/process/impl/JavaSakerProcessBuilder.java
@@ -71,6 +71,9 @@ public class JavaSakerProcessBuilder extends SakerProcessBuilderBase {
 			//default to Redirect.PIPE
 		}
 
+		//redirect the std in to null, to signal that there's no input coming from there
+		pb.redirectInput(NULL_FILE);
+
 		Process proc = pb.start();
 		return new JavaSakerProcess(proc, stdoutconsumer, stderrconsumer);
 	}
@@ -111,6 +114,10 @@ public class JavaSakerProcessBuilder extends SakerProcessBuilderBase {
 		public void processIO() throws IllegalStateException, IOException {
 			//synchronize, only a single client can process the IO at once
 			synchronized (this) {
+				//close the stdin of the process as we currently don't handle it
+				//we already redirected to NULL, however, close it if non-null just in case.
+				IOUtils.close(proc.getOutputStream());
+
 				InputStream stderrstream = proc.getErrorStream();
 				InputStream stdoutstream = proc.getInputStream();
 				if (stderrstream == null) {
